8b92394cff9d8edd3f8fe4e641388e37ce8378e1
Alisson Joaquim <alisson.oliveira@sou.ufmt.br>
2024-03-22 14:44:36 -0400

    add:produtos

--- manipulaProduto.py
+++ manipulaProduto.py
@@ -32,3 +32,45 @@ def editar() -> bool:
             campos = ["Id", "Setor", "Nome", "Preco", "Validade", "Quantidade"]
             return mcsv.gravarDados("Data/Produtos.csv", campos, produtos)
     return False
+
+def estoqueSetor(setor: str) -> list:
+    '''
+    Retorna uma lista de produtos de um determinado setor
+
+    Parâmetros
+    ----------
+    setor : str
+        Setor dos produtos a serem listados
+
+    Retorno
+    -------
+    Retorna uma lista de dicionários com os produtos do setor informado
+    '''
+    produtos = carregar()
+    return [p for p in produtos if p["Setor"] == setor]
+
+def estoqueBaixo() -> list:
+    '''
+    Retorna uma lista de produtos com estoque baixo
+
+    Retorno
+    -------
+    Retorna uma lista de dicionários com os produtos com estoque baixo
+    '''
+    produtos = carregar()
+    return [p for p in produtos if p["Quantidade"] < 10]
+
+def maisVendido() -> dict:
+    '''
+    Retorna o produto mais vendido
+
+    Retorno
+    -------
+    Retorna um dicionário com o produto mais vendido
+    '''
+    produtos = carregar()
+    maisVendido = produtos[0]
+    for p in produtos:
+        if p["Quantidade"] < maisVendido["Quantidade"]:
+            maisVendido = p
+    return maisVendido
