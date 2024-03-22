8a67ef164968d08a38d304b1a91d100882e39a57
Alisson Joaquim <alisson.oliveira@sou.ufmt.br>
2024-03-22 13:47:07 -0400

    add:menu Produtos

--- apresentacao.py
+++ apresentacao.py
@@ -1,6 +1,8 @@
 from os import system, name
 
 #################################################################
+
+
 def limpaTela():
     '''
     Limpa a tela de acordo com o systema operacional (Windows ou Linux)
@@ -10,33 +12,47 @@ def limpaTela():
     else:
         _ = system("clear")
 
-#################################################################        
+#################################################################
 
-def MenuPrincipal() -> str :
+
+def MenuPrincipal() -> str:
     '''
     Exemplo de Menu principal para o sistema
-    
+
     Retorno    
     -------
     Retorna válida escolhida
 
     '''
-    opcoes = [1,2,3,9]
+    opcoes = [1, 2, 3, 9]
     opcao = 10
     while opcao not in opcoes:
         limpaTela()
         print("#"*20)
-        print("1.Venda\n2.Clientes\n3.Produto\n9.Sair")    
+        print("1.Venda\n2.Clientes\n3.Produto\n9.Sair")
         print('#'*20)
-        opcao = int(input("Opção -> "))            
+        opcao = int(input("Opção -> "))
     return opcao
 
-#################################################################    
+#################################################################
 
-def CadastrarProduto() -> dict :
+
+def MenuProduto() -> str:
+    opcoes = [1, 2, 3, 4, 5, 9]
+    opcao = 10
+    while opcao not in opcoes:
+        limpaTela()
+        print("#"*20)
+        print(
+            "1.Cadastrar novo produto\n2.Atualizar informações do produto\n3.Estoque por setor\n4.Produtos com estoque baixo\n5.Produtos mais vendidos\n9.Sair")
+        print("#"*20)
+        opcao = int(input("Opção -> "))
+
+
+def CadastrarProduto() -> dict:
     '''
     Exibe uma interface para ler os dados de um produto
-    
+
     Retorno
     -------
     Retorno um dicionário com os campos e dados de um produto
@@ -58,11 +74,12 @@ def CadastrarProduto() -> dict :
     produto['Quantidade'] = int(input("Quantidade de produto no estoque: "))
     print("="*30)
     return produto
-    
+
+
 def EditarProduto() -> int:
     '''
     Exibe uma interface para ler o id do produto a ser editado
-    
+
     Retorno
     -------
     Retorna o id do produto a ser editado
