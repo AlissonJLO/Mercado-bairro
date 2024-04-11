022993af1254bce5cbff55e6e175f27f27d46765
Alisson Joaquim lara oliveira <alissonloaquim@hotmail.com>
2024-04-11 00:09:49 -0400

    correçao do id

--- manipulaProduto.py
+++ manipulaProduto.py
@@ -22,7 +22,7 @@ def cadastrar() -> bool:
     listaProdutos = carregar()
     prod = apresentacao.CadastrarProduto()
     listaProdutos.append(prod)
-    campos = ["Id", "Setor", "Nome", "Preco", "Validade", "Quantidade"]
+    campos = ["Id-Produto", "Setor", "Nome", "Preco", "Validade", "Quantidade"]
     return mcsv.gravarDados("Data/Produtos.csv", campos, listaProdutos)
 
 
@@ -33,11 +33,11 @@ def editar() -> bool:
     produtos = carregar()
     produto_encontrado = False
     for produto in produtos:
-        if produto["Id"] == id:
+        if produto["Id-Produto"] == id:
             produto_encontrado = True
             produto_atualizado = apresentacao.CadastrarProduto()
             produto.update(produto_atualizado)
-            campos = ["Id", "Setor", "Nome", "Preco", "Validade", "Quantidade"]
+            campos = ["Id-Produto", "Setor", "Nome", "Preco", "Validade", "Quantidade"]
             sucesso = mcsv.gravarDados("Data/Produtos.csv", campos, produtos)
             print("Dados gravados com sucesso." if sucesso else "Falha ao gravar dados.")
             return sucesso
@@ -63,7 +63,7 @@ def produtoSetor():
     if len(produtos_setor) > 0:
         print(f"Produtos do setor {setor}:")
         for produto in produtos_setor:
-            print(f"ID: {produto['Id']}, Nome: {produto['Nome']}, Preço: R${produto['Preco']}, Validade: {produto['Validade']}, Quantidade em estoque: {produto['Quantidade']}")
+            print(f"ID: {produto['Id-Produto']}, Nome: {produto['Nome']}, Preço: R${produto['Preco']}, Validade: {produto['Validade']}, Quantidade em estoque: {produto['Quantidade']}")
     else:
         print(f"Não há produtos no setor {setor}.")
 
@@ -83,7 +83,7 @@ def maisVendidos():
         venda['Data'], "%d/%m/%Y") >= tres_dias_atras]
 
     # Mapear ID do produto para o nome do produto
-    nome_por_id = {produto['Id']: produto['Nome'] for produto in produtos}
+    nome_por_id = {produto['Id-Produto']: produto['Nome'] for produto in produtos}
 
     # Contabilizar a quantidade vendida de cada produto pelo nome
     quantidade_por_produto = {}
@@ -113,58 +113,7 @@ def maisVendidos():
     return True
 
 
-def exibir_info_produto(id_produto: str, quantidade_desejada: int, lista_compras: list, cpf):
-    '''
-    Exibe informações do produto conforme o ID e a quantidade desejada digitados
-    e adiciona o produto à lista de compras se a quantidade em estoque for suficiente
-    -------
-    retorna lista de compras
-    '''
-    produtos = carregar()
-    produto_encontrado = False
-    for linha in produtos:
-        if linha['Id'] == id_produto:
-            produto_encontrado = True
-            quantidade_estoque = int(linha['Quantidade'])
-            if quantidade_desejada <= quantidade_estoque:
-                print("Nome do Produto:", linha['Nome'])
-                print("Preço do Produto:", linha['Preco'])
-                print("Quantidade em Estoque antes da compra:", quantidade_estoque)               
-                
-                # Adiciona o produto à lista de compras
-                lista_compras = []
-                item_Nome = linha['Nome']
-                item_Preco = linha['Preco']
-                item_Quantidade = quantidade_desejada 
-                item_lista = [item_Nome, item_Preco, item_Quantidade ]
-                lista_compras.append(item_lista)
-                print("Produto adicionado à lista de compras.")
-                
-                # Atualiza a quantidade em estoque no arquivo "Produtos.csv"
-                linha['Quantidade'] = quantidade_estoque - quantidade_desejada
-                campos = ["Id", "Setor", "Nome", "Preco", "Validade", "Quantidade"]
-                mcsv.gravarDados("Data/Produtos.csv", campos, produtos)
-                 
-                #registro itens compra
-                registro_item = []
-                itensCompra = mitc.carregar()
-                id_Venda = mvend.gerar_id_venda()
-                preco_uni = linha['Preco']
-                preco_total = (float(linha['Preco'])) * quantidade_desejada
-                registro_item = [id_Venda, cpf, id_produto, quantidade_desejada, preco_uni, preco_total]
-                #adiciona registro item à ItensCompra.csv
-                itensCompra.append(registro_item)
-                campos = ["Id-Venda", "CPF", "Id-Produto", "Quantidade", "Preço-Unitário", "Preço-Total"]
-                mcsv.gravarDados("Data/ItensCompra.csv", campos, itensCompra)
-                break  # produto encontrado, interrompe o loop
-                                                   
-            else:
-                print("Não há quantidade suficiente do produto:", linha['Nome'])
-                break
-        if not produto_encontrado:
-            print("Produto não encontrado.")
 
-    return lista_compras
 
 ''''
 mitc.carregar() retorna uma lista de dicionários com os itens de compra
--- manipulaVenda.py
+++ manipulaVenda.py
@@ -30,43 +30,28 @@ def novaVenda() -> bool:
     venda = apresentacao.efetuar_venda()
     vendas = carregar()
     vendas.append(venda)
-    #salva vednda no arquivo vendas.csv
-    campos = ['Id-Venda','CPF','Data', 'Total', 'Quantidade-Produtos']
+    # salva vednda no arquivo vendas.csv
+    campos = ['Id-Venda', 'CPF', 'Data', 'Total', 'Quantidade-Produtos']
     return mcsv.gravarDados("Data/Vendas.csv", campos, vendas)
-
-
-def gerar_id_venda():
-    '''
-    busca id venda em "Vendas.csv"
-    ------
-    retorna gera um id baseado na ultima venda +1
-    '''
-    vendas = carregar()
-    if not vendas:
-        return 1  # Se não houver vendas, o próximo ID será 1
-    
-    ultimo_dict = vendas[-1]
-    ultimo_id = ultimo_dict.get("Id-Venda", 0)  # Obtém o último ID de venda, se não existir, assume 0
-    return ultimo_id + 1  # Incrementa o último ID de venda em 1
 
 
 def vendas_do_cliente():
     apresentacao.limpaTela()
     # verifica se o CPF é cadastrado
     cpf = apresentacao.ler_cpf()
-    clientes = mcli.carregar()            
+    clientes = mcli.carregar()
     cadastro = mcli.checar_cadastro(clientes, cpf)
-    if cadastro:            
-        #cadastro encontrado
+    if cadastro:
+        # cadastro encontrado
         vendas = carregar()
-        vendas_cliente = listar_vendas(vendas,cpf)
+        vendas_cliente = listar_vendas(vendas, cpf)
         print(vendas_cliente)
         input("enter para voltar\n")
     else:
         # CPF não encontrado, redirecionar para cadastro
         print("CPF fornecido não cadastrado.\nRedirecionando para cadastro...\n")
-        time.sleep(3)           
-        mcli.cadastrarCli()      
+        time.sleep(3)
+        mcli.cadastrarCli()
 
 
 def listar_vendas(vendas, cpf):
